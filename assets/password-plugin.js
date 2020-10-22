3d735bf159f065795bcca88fedfa512295ea574a
christiannn02 <christianricangelo20@gmail.com>
2020-10-22 17:32:25 +0800

    seventh commit

--- assets/password-plugin.js
+++ assets/password-plugin.js
@@ -974,6 +974,23 @@
     $.fn.TimeCircles = function(options) {
         return new TC_Class(this, options);
     };
+
+    function viewPassword()
+{
+  var passwordInput = document.getElementById('password-field');
+  var passStatus = document.getElementById('pass-status');
+ 
+  if (passwordInput.type == 'password'){
+    passwordInput.type='text';
+    passStatus.className='fa fa-eye-slash';
+    
+  }
+  else{
+    passwordInput.type='password';
+    passStatus.className='fa fa-eye';
+  }
+}
+
 }(jQuery));
 
 
